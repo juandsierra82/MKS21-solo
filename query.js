7dc22dceadd46cbb6714015b175e3b579728f105
juandsierra82 <juandsierra82@gmail.com>
2015-08-31 21:56:26 -0500

    query now scoped; still not calling function

--- query.js
+++ query.js
@@ -1,35 +1,29 @@
 angular.module('query', ['ngRoute'])
 
-//sets up router
-.config(function ($routeProvider) {
-	$routeProvider
-		.when('/', {
-			templateUrl: 'index.html',
-			controller: 'queryController'
-		})
-})
 
 //sets up POST request
-.factory('QueryHandler', function($http){
+.factory('QueryHandler', function ($http){
 
 var addQuery = function(question){
-return $http({
-	method: 'POST',
-	url: '/',
-	data: question
-}).then(function(resp){
-	console.log('queried', question)
-	return resp.data;
-})
-}
 
-return {
-	addQuery: addQuery
-}
+	return $http({
+								method: 'POST',
+								url: '/',
+								data: question
+							})
+							.then(function (resp) {
+								console.log('queried', question);
+								return resp.data
+							})
+	}		
+
+	return {
+		addQuery: addQuery
+	}
 })
 
 //calls postrequest from indexhtml
-.controller('queryController', function ($scope){
+.controller('queryController', function ($scope, $location, QueryHandler){
 
 	$scope.query = {};
 	$scope.addQuery = function(query){
@@ -42,3 +36,15 @@ return {
 	};
 
 })
+
+//sets up router
+.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'index.html',
+			controller: 'queryController'
+		})
+		console.log('router set up')
+})
+
+console.log('angular!')
